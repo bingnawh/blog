d69892b056bcad6b1b99832ed42b4fc2be30fcf3
tbhbhbh <tanbinghwang@gmail.com>
2020-11-14 17:23:07 +0800

    Remove unused code

--- blog-generator/main.go
+++ blog-generator/main.go
@@ -1,21 +1,6 @@
 package main
 
-import (
-	"log"
-)
-
 func main() {
-	log.Println("Generating blog...")
 	GenerateBlog()
-
-	//	fs := http.FileServer(http.Dir("."))
-	//	http.Handle("/", fs)
-	//
-	//	log.Println("Listening on :3000...")
-	//
-	//	err := http.ListenAndServe(":3000", nil)
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
 
 }
